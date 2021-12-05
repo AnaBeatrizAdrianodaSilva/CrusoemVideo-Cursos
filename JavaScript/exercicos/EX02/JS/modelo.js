1a14c40125cfca4c6c1feb29e2f9654b3da5ab50
Raven <ana.github0@gmail.com>
2021-12-05 15:04:22 -0300

    finalização do exercício 02 do curso de JS

--- JavaScript/exercicos/EX02/JS/modelo.js
+++ JavaScript/exercicos/EX02/JS/modelo.js
@@ -11,12 +11,42 @@ function Verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
+        var  img = document.createElement('img')
+        img.setAttribute('id', 'foto')
 
         if (fsex[0].checked) {
             genero = 'Homem'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'bebe-m.png')
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'adolescent-m.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adulto-m.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idoso-m.png')
+            }
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'bebe-f.png')
+            } else if (idade < 21) {
+                // Adolescente
+                img.setAttribute('src', 'adolescent-f.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adulto-f.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idoso-f.png')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
