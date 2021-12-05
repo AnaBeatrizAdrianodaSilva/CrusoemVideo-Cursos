f675f1e5b2343d36ad99e41b372abb6bad541446
Raven <ana.github0@gmail.com>
2021-12-05 16:29:06 -0300

    exercícios  012 e 013 do curso js

---
+++ JavaScript/aulas/ex012/repetições while-do.js
@@ -0,0 +1,33 @@
+
+// While
+
+/*
+var c = 1
+
+while (c <= 6) {
+    console.log(`Passo ${c}`) 
+    c++
+}
+*/
+        
+//  -------------------------------------------------------------------------------------------------------------------------
+
+// Do (While)
+
+/*
+var c = 1
+
+do {
+    console.log(`Passo ${c}`) 
+    c++
+} while (c <= 6) 
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
