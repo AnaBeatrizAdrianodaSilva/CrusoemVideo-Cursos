b4293f37e2ae5af3e0b1d126784ae9857d4792cd
Ana Beatriz <ana.github0@gmail.com>
2023-02-14 17:19:10 -0300

    HTML pronto. Separação do que tem que fazer no arquivo main.js.

---
+++ JavaScript/exercicos/EX05/main.js
@@ -0,0 +1,30 @@
+// Função que pega o valor do input do número entre 1 e 100
+
+    // Pegar o valor do input
+
+    // Adiciona na div
+
+    // Verifica se já tem o mesmo número
+
+
+
+// Função de finalização que mostra alguns dados com base no que tem dentro da div
+
+    // Mostrar na tela algumas informações
+
+        // Quantos números foram cadastrados
+
+        // O maior número cadastrado
+
+        // O menor número cadastrado
+
+        // A soma de todos os números cadastrados
+
+        // A média de todos os números cadastrados
+
+
+
+
+// Extra
+
+// Função que limpa tudo
