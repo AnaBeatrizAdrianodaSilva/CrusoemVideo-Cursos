53ee9cb2ef315b1f040c7969225853df0c4ee4dd
Ana Beatriz Adriano da Silva <ana.github0@gmail.com>
2023-02-14 21:17:59 -0300

    Tentativa de realizar o exercício. Problema com o appendChild que não quer adicionar o elemento

--- JavaScript/exercicos/EX05/main.js
+++ JavaScript/exercicos/EX05/main.js
@@ -1,3 +1,18 @@
+let number = document.querySelector('#number');
+let list = document.querySelector('select#list');
+let val = [];
+
+function addNumberToAList() {
+    
+    val.push(Number(number.value));
+    let item = document.createElement('option');
+    item.text = `Número ${number.value} adicionado`;
+    // item.text = "Número " + number.value + " adicionado";
+    list.appendChild(item); // PORRA DE appendChild NÃO FUNCIONA, O QUE NÃO FAZ O MENOR SENTIDO
+
+}
+
+
 // Função que pega o valor do input do número entre 1 e 100
 
     // Pegar o valor do input
